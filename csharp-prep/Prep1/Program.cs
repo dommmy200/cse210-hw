20f47b8764947590c686d15f1e3a4f96709da9bd
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-09-15 00:12:33 +0100

    Finished stretch Prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What's your Last name?");
+        Console.Write("What's your Last name? ");
         string lastName = Console.ReadLine();
 
-        Console.Write("What's your first name?");
+        Console.Write("What's your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName}{lastName}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please, enter your grade. ");
+        string gradeInString = Console.ReadLine();
+        float grade = float.Parse(gradeInString);
+
+        if (grade >= 90)
+        {
+            Console.Write("You scored an A ");
+        }
+        
+        else if (grade >= 80 && grade < 90)
+        {
+            float gradeRemainder = grade - 80;
+            if (gradeRemainder >= 7)
+            {
+                Console.Write("You scored a B+ ");
+            }
+            else if (gradeRemainder < 3)
+            {
+                Console.Write("You scored a B- ");
+            }
+            else {Console.Write("You scored a B ");}
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+             float gradeRemainder = grade - 70;
+            if (gradeRemainder >= 7)
+            {
+                Console.Write("You scored a C+ ");
+            }
+            else if (gradeRemainder < 3)
+            {
+                Console.Write("You scored a C- ");
+            }
+            else {Console.Write("You scored a C ");}
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+             float gradeRemainder = grade - 60;
+            if (gradeRemainder >= 7)
+            {
+                Console.Write("You scored a D+ ");
+            }
+            else if (gradeRemainder < 3)
+            {
+                Console.Write("You scored a D- ");
+            }
+            else {Console.Write("You scored a D ");}
+        }
+        else if (grade < 60)
+        {
+            Console.Write("You scored an F ");
+        }
     }
 }
