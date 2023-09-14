dff4ead3a5bfe0b84d985abc065e42d7bd76c3bc
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-09-14 20:57:31 +0100

    First Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your Last name?");
+        string lastName = Console.ReadLine();
+
+        Console.Write("What's your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName}{lastName}.");
     }
 }
