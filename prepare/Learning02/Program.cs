4266e05462eba8e15824565bc250c9d4b5091d0f
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-09-28 21:32:36 +0100

    Pushing the modified main file

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,40 @@
-using System;
+using MyLearning02.MyJobs;
+using MyLearning02.MyResume;
 
-class Program
+namespace MyLearning02
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        static void Main(string[] args)
+        {
+            Job job1 = new()
+            {
+                _jobTitle = "Software Engineer",
+                _company = "Microsoft",
+                _startYear = 2009,
+                _endYear = 2021
+            };
+
+            Job job2 = new()
+            {
+                _jobTitle = "Web Designer",
+                _company = "Apple",
+                _startYear = 2011,
+                _endYear = 2023
+            };
+
+            Resume myResume = new()
+            {
+                _name = "John Doe"
+            };
+
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+
+            myResume.Display();
+
+
+        }
     }
 }
