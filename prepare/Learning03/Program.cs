9df16525bfb07e9a1c3bbed9495a72d3671561b9
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-10-11 11:50:38 +0100

    Completed prepare wk05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using Program1.Fraction1;
 
-class Program
+namespace Program {
+    class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction(7, 2);
+        Fraction f2 = new Fraction(3);
+        
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
     }
+    
+}
 }
