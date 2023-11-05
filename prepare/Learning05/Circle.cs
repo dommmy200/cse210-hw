dd3e0c9787f3fb9ba42c031dca25e82679e9de75
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-11-05 16:22:28 +0100

    Completed learning05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ShapeAndColors {
+    public class Circle : Shape {
+        private double _radius;
+        public Circle(double radius, string color) : base(color) {
+            _radius = radius;
+        }
+        public override double GetArea() {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+namespace ShapeAndColors {
+    class Program {
+        static void Main(string[] args) {
+            Square square = new Square(4.5d, "Blue");
+            double squ = square.GetArea();
+            string squCol = square.GetColor();
+            Rectangle rectangle = new Rectangle(3.6d, 2.0d, "Yellow");
+            double rec = rectangle.GetArea();
+            string recCol = rectangle.GetColor();
+            Circle circle = new Circle(8.4d, "Green");
+            double cir = circle.GetArea();
+            string cirCol = circle.GetColor();
+
+            List<Shape> shapes = new List<Shape>(5) {
+                square, rectangle, circle
+            };
+            foreach (Shape shape in shapes){
+                string col = shape.GetColor();
+                double dou = shape.GetArea();
+                Console.WriteLine($"{shape} = '{col}' {dou}");
+            }
+
+            Console.WriteLine($"Area of Square: {squ} and Color: {squCol}\nArea of Rectangle: {rec} and Color: {recCol}\nArea of Square: {cir} and Color: {cirCol}");
+        }
     }
 }
