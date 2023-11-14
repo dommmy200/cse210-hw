b73e87ff55b666c8c4a18be19c1bc078b884e50e
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-11-14 19:53:55 +0100

    Near completed Eternal Goal prj

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -2,22 +2,22 @@ using System;
 using System.IO;
 
 namespace EternalGoal {
-public class EternalGoal : Goal {
-    public EternalGoal(string goalName, string description, int point) : base(goalName,  description, point){
+    public class EternalGoal : Goal {
+        public EternalGoal(string goalName, string description, int point) : base(goalName,  description, point){
+        }
+        public override void DisplaySubclassObjects(int serialNumber) {
+            Console.WriteLine($"{serialNumber}. {GetGoalName()}, ({GetGoalDescription()})");
+        }
+        public override string SaveGoal() {
+            string goalStructure = $@"{GetClassName()}: {GetGoalName()}, {GetGoalDescription()}, {GetPoint()}";
+            return goalStructure;
+        }
+        public override string LoadGoal() {
+            return "Load Goals";
+        }
+        public override void RecordGoalEvent(HelpClass helper1) {
+            int point = GetPoint();
+            helper1.AddPoints(point);
+        }
     }
-    public override void DisplaySubclassObjects(int serialNumber) {
-        Console.WriteLine($"{serialNumber}. {GetGoalName()}, ({GetGoalDescription()})");
-    }
-    public override string SaveGoal() {
-        string goalStructure = $@"{GetClassName()}: {GetGoalName()}, {GetGoalDescription()}, {GetPoint()}";
-        return goalStructure;
-    }
-    public override string LoadGoal() {
-        return "Load Goals";
-    }
-    public override void RecordGoalEvent(HelpClass helper1) {
-        int point = GetPoint();
-        helper1.AddPoints(point);
-    }
-}
 }
