8cfaa73e5c836e4703a0d06db3e4e268703920b5
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-12-07 16:28:39 +0100

    StaticClassRemv StaticMethAdd

--- final/FinalProject/DebtManagement.cs
+++ final/FinalProject/DebtManagement.cs
@@ -1,17 +1,17 @@
 using System;
 
 namespace FinancialPrudence {
-    public static class DebtManagement {
+    public class DebtManagement {
         // private string _name;
         // private Information _info = new Information();
-        // private Helper _helper1 = new();
-        private static Statement _statement1 = new IncomeStatement();
+        // private Helper _helper = new Helper();
+        private Statement _statement1 = new IncomeStatement();
         // Debt management is done here by amount reduction or deletion
         // public DebtManagement(string name) {
         //     _name = name;
         // }
-        private static void ReduceDebt() {
-            List<Statement> state = Helper.GetListOfObjects();
+        private void ReduceDebt() {
+            List<Statement> state = Helper.GetAllObjectsList();
             for (int i = 0; i < state.Count; i++) {
                 string type = state[i].GetType().Name;
                 if (type.Contains("expenses")) {
@@ -31,11 +31,11 @@ namespace FinancialPrudence {
         }
         // Debt management is done here by increasing income
         // and expenses reduction or deletion
-        private static void IncreaseIncome(Statement statement) {
+        private void IncreaseIncome(Statement statement) {
             statement.GetStatement();
         }
         // Select between Debt management and 
-        public static void ManageIncomeAndExpense() {
+        public void ManageIncomeAndExpense() {
             bool quit = false;
             while (quit) {
                 int num = Information.ReduceOrIncreaseInfo();
